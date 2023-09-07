5260e76c6166ef67323c1b35f7503e36837a52d4
davereichert <92051098+davereichert@users.noreply.github.com>
2023-09-07 15:36:45 +0200

    klasse Translator mit Klasse

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,19 +13,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using gruppe_2_easyword;
 
-namespace gruppe_2_easyword
+namespace Transalto
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        Translator translator;
+        string currentWord;  // Dies speichert das aktuelle Wort, das übersetzt werden soll
+
         public MainWindow()
         {
             InitializeComponent();
-            MessageBox.Show("Test");
 
+            // Laden Sie die CSV-Datei und erstellen Sie den Übersetzer
+            string[] data = File.ReadAllLines("C:\\Users\\david\\OneDrive\\Desktop\\Arbeit.csv");
+            translator = new Translator(data);
+        }
+
+        // Dies könnte ein Ereignishandler sein, der ausgelöst wird, wenn Sie die Übersetzungsrichtung ändern wollen
+        private void ChangeTranslationDirection(object sender, RoutedEventArgs e)
+        {
+            // Beispiel: Holen Sie sich das nächste Wort aus der CSV-Datei (dies kann erweitert werden)
+            currentWord = data[0].Split(';')[0];  // Nehmen Sie das erste Wort als Beispiel
+
+            // Zeigen Sie das Wort im TextBlock an
+            WordToTrans.Text = currentWord;
+        }
+
+        private void TestResu(object sender, RoutedEventArgs e)
+        {
+            string userTranslation = WordTransResu.Text;
+            string result = translator.CheckXToY(currentWord, userTranslation);  // Für Deutsch nach Englisch
+                                                                                 // Oder: 
+                                                                                 // string result = translator.CheckYToX(currentWord, userTranslation);  // Für Englisch nach Deutsch
+
+            // Zeigen Sie das Ergebnis an (z.B. in einem MessageBox oder einem anderen TextBlock)
+            MessageBox.Show(result);
         }
     }
+
 }
---
+++ Translator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace gruppe_2_easyword
+{
+    public class Translator
+    {
+        private Dictionary<string, List<string>> de_to_eng = new Dictionary<string, List<string>>();
+        private Dictionary<string, List<string>> eng_to_de = new Dictionary<string, List<string>>();
+
+        public Translator(string[] data)
+        {
+            foreach (string entry in data)
+            {
+                string[] parts = entry.Split(';');
+                string x = parts[0];
+                string y = parts[1];
+
+                if (!de_to_eng.ContainsKey(x))
+                    de_to_eng[x] = new List<string>();
+                de_to_eng[x].Add(y);
+
+                if (!eng_to_de.ContainsKey(y))
+                    eng_to_de[y] = new List<string>();
+                eng_to_de[y].Add(x);
+            }
+        }
+
+        private string Check(string word, Dictionary<string, List<string>> wordDict, string userTranslation)
+        {
+            string translations = string.Join(";", wordDict[word]);
+
+            if (wordDict[word].Contains(userTranslation))
+                return $"Richtig! Mögliche Übersetzungen von {word} sind: {translations}";
+            else
+                return $"Falsch! Mögliche Übersetzungen von {word} sind: {translations}";
+        }
+
+
+        public void CheckdeToeng(string word)
+        {
+            Check(word, de_to_eng);
+        }
+
+        public void CheckYToX(string word)
+        {
+            Check(word, eng_to_de);
+        }
+    }
+}
